1a520fe88c17501d903a5c6352cc4e8c9828d5f5
nrodriguezcuellar <snrodriguezcuellar@outlook.com>
2025-04-27 00:25:29 +0200

    add tool

--- packages/mcp/src/index.ts
+++ packages/mcp/src/index.ts
@@ -1,24 +1,42 @@
+#!/usr/bin/env node
+
 import { McpServer } from "@modelcontextprotocol/sdk/server/mcp.js";
 import { StdioServerTransport } from "@modelcontextprotocol/sdk/server/stdio.js";
 import packageJson from "../package.json" with { type: "json" };
 import { isDaemonRunning } from "@nicorodri/tempo-daemon";
-import z from "zod";
 
 const server = new McpServer({
   name: "Tempo-MCP-Server",
   version: packageJson.version,
+  description: "Tempo MCP Server",
 });
 
-server.tool("check tracking status", () => {
-  return {
-    content: [
-      {
-        type: "text",
-        text: isDaemonRunning() ? "Daemon is running" : "Daemon is not running",
-      },
-    ],
-  };
-});
+server.tool(
+  "check-tracking-status",
+  "Check whether the tempo tracker is running at the moment",
+  () => {
+    return {
+      content: [
+        {
+          type: "text",
+          text: isDaemonRunning() ? "Tempo is running" : "Tempo is not running",
+        },
+      ],
+    };
+  },
+);
 
-const transport = new StdioServerTransport();
-await server.connect(transport);
+async function main() {
+  console.log(`Starting Tempo MCP Server v${packageJson.version}...`);
+  const transport = new StdioServerTransport();
+  await server.connect(transport);
+}
+
+main()
+  .then(() => {
+    console.log("Server started!");
+  })
+  .catch((error) => {
+    console.error("Error starting server:", error);
+    process.exit(1);
+  });
